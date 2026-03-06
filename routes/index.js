fa94f517dddf26e8383968d728ddf8f2ef278a4e
PEKSSS <126037599+PEKSSS@users.noreply.github.com>
2026-03-06 21:45:50 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,7 @@ const userController=require('../controllers/userController');
 //route to get all users
 router.get('/users/',userController.getAllUsers);
 //route to search a user by id
-router.get('/users/:id',userController.getUserById);
-//rouse to create a new user
 router.post('/users',userController.createUser);
 //route to edit a user
-router.put('/users',userController.updateUser);
-//route to delete a user
-router.delete('/users',userController.deleteUser);
+
 module.exports=router;
