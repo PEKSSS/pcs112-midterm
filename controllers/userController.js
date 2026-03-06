93079b42cbd9b8fc790b026f623d95691b56a0a3
PEKSSS <126037599+PEKSSS@users.noreply.github.com>
2026-03-06 21:38:44 +0800

    Refactor userController to remove commented code

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,19 +8,6 @@ exports.getAllUsers=(req,res)=>{
     });
 };
 
-// Search a user by id
-// CRUD - Report
-// exports.getUserById=(req,res)=>{
-//     const id=req.params.id;
-//     connection.query('SELECT * FROM student_enrollments WHERE id=?', [id], (err, rows, fields)=>{
-//         if(err) throw err;
-//         if(rows.length>0)
-//             res.json(rows);
-//         else
-//             res.status(404).json({message: "User not found"});
-//     });
-// }
-
 //Create a new user
 //CRUD - Create
 exports.createUser=(req,res)=>{
@@ -30,32 +17,4 @@ exports.createUser=(req,res)=>{
         res.json({message: 'User created successfully', userId: result.insertId});
     })
 }
-
-// //Edit a user
-// //CRUD - Update
-
-// exports.updateUser=(req,res)=>{
-//     const {id, productName, category, stockCount, locationCode, lastUpdated}=req.body;
-//     connection.query('UPDATE student_enrollments SET productName=?, category=?, stockCount=?, locationCode=? WHERE id=?', [productName, category, stockCount, locationCode, id], (err, result)=>{
-//         if(err) throw err;
-//         if(result.affectedRows>0)
-//             res.json({message: 'User updated successfully'});
-//         else
-//             res.status(404).json({message: 'User not found'});
-//         })
-//     }
-
-// //Delete a user
-// //CRUD - Delete
-// exports.deleteUser=(req,res)=>{
-//     const id=req.body.id;
-//     connection.query('DELETE FROM student_enrollments WHERE id=?', [id], (err, result)=>{
-//         if(err) throw err;
-//         if(result.affectedRows>0)
-//             res.json({message: 'User deleted successfully'});
-//         else
-//             res.status(404).json({message: 'User not found'});
-//     })
-// }
-
 
